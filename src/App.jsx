592159dc821074f5cac7bc2c45a44b0a8dc4ffe0
ArtemBash <temon-bash@yandex.ru>
2024-03-22 19:42:47 +0700

    adds

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <span className='icon entypo-plus scnd-font-color'></span>
         </a> */}
         <div className='profile-picture big-profile-picture clear'>
-          <img width='150px' alt='img' src={avatar} />
+          <img alt='img' width='150px' src={avatar} />
         </div>
         <h1 className='user-name'>Artem Basharin</h1>
         <div className='profile-description'>
@@ -84,7 +84,11 @@ function App() {
         </li>
         <li className='icon'>
           <a href='https://career.habr.com/artembash'>
+<<<<<<< Updated upstream
             <img src={habr} className='icon_habr' alt='img' />
+=======
+            <img src={habr} alt='img' className='icon_habr' />
+>>>>>>> Stashed changes
           </a>
         </li>
       </div>
@@ -254,14 +258,22 @@ function App() {
       <div className='block'>
         <h2 className='titular'>CERTIFICATION</h2>
         <div className='block_cert'>
+<<<<<<< Updated upstream
           <img src={fcc} className='icon_small' alt='img' />
+=======
+          <img alt='img' src={fcc} className='icon_small' />
+>>>>>>> Stashed changes
           <a href='https://freecodecamp.org/certification/artembash/responsive-web-design'>
             Responsive Web-design
           </a>
         </div>
 
         <div className='block_cert'>
+<<<<<<< Updated upstream
           <img src={fcc} className='icon_small' alt='img' />
+=======
+          <img alt='img' src={fcc} className='icon_small' />
+>>>>>>> Stashed changes
           <a href='https://www.freecodecamp.org/certification/artembash/javascript-algorithms-and-data-structures'>
             JS Algorithms and Data Structures
           </a>
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,86 +1,52 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 function Weather() {
   const apiLocationKey = "a9e48c70-8b22-11ed-8d13-bd165d1291e3";
   const apiWeatherKey = "d21939b2a613cb8cf21acd6afd167bf5";
-  // const [details, setDetails] = useState(null);
-  const [weatherUrl, setWeatherUrl] = useState("");
+  // const [weatherUrl, setWeatherUrl] = useState("");
   const [isWeatherLoading, setIsWeatherLoading] = useState(true);
 
-  // const [weatherIconCode, setWeatherIconCode] = useState("");
+  axios.get(`https://geolocation-db.com/json/${apiLocationKey}`).then((res) => {
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${res.data.latitude}&lon=${res.data.longitude}&appid=${apiWeatherKey}`;
+    console.log(weatherUrl);
 
-  // const getUserGeolocationDetails = () =>
-  //   fetch(`https://geolocation-db.com/json/${apiLocationKey}`)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setDetails(data);
-  //       setWeatherUrl(
-  //         `https://api.openweathermap.org/data/2.5/weather?lat=${details.latitude}&lon=${details.longitude}&appid=${apiWeatherKey}`
-  //       );
-  //     });
-  // getUserGeolocationDetails();
-
-  useEffect(() => {
-    fetch(`https://geolocation-db.com/json/${apiLocationKey}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setWeatherUrl(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${data.latitude}&lon=${data.longitude}&appid=${apiWeatherKey}`
-        );
-        console.log(weatherUrl);
-      });
-  }, []);
-
-  // console.log(details);
-
-  // function DateToday() {
-  //   const today = new Date();
-  //   const yyyy = today.getFullYear();
-  //   let mm = today.getMonth() + 1; // Months start at 0!
-  //   let dd = today.getDate();
-  //   if (dd < 10) dd = "0" + dd;
-  //   if (mm < 10) mm = "0" + mm;
-  //   const formattedToday = dd + "/" + mm + "/" + yyyy;
-  //   return formattedToday;
-  // }
-
-  // Город погода которого нужна
-  // Формируем url для GET запроса
-
-  // if (details !== null) {
-  //   setWeatherUrl(
-  //     `https://api.openweathermap.org/data/2.5/weather?lat=${details.latitude}&lon=${details.longitude}&appid=${apiKey}`
-  //   );
-  // }
-
-  useEffect(() => {
     axios.get(weatherUrl).then((res) => {
-      // Вывод температуры
-      document.querySelector(".temp").innerHTML =
+      console.log(res.data);
+      document.querySelector(".temperature").innerHTML =
         Math.trunc(res.data.main.temp) - 273;
-      // Вывод влажности
       document.querySelector(".humidity").innerHTML = res.data.main.humidity;
-      // Вывод скорости ветра
       document.querySelector(".wind").innerHTML = res.data.wind.speed;
       document.querySelector(
         ".weather_icon"
       ).src = `https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`;
       setIsWeatherLoading(false);
     });
-  }, [weatherUrl]);
+  });
 
-  // const getWeatherData = () => {
-  //   axios.get(weatherUrl).then((res) => {
-  //     // Вывод температуры
-  //     document.querySelector(".temp").innerHTML = res.data.main.temp;
-  //     // Вывод влажности
-  //     document.querySelector(".humidity").innerHTML = res.data.main.humidity;
-  //     // Вывод скорости ветра
-  //     document.querySelector(".wind").innerHTML = res.data.wind.speed;
-  //   });
-  // };
+  // useEffect(() => {
+  //   fetch(`https://geolocation-db.com/json/${apiLocationKey}`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${data.latitude}&lon=${data.longitude}&appid=${apiWeatherKey}`;
+  //       console.log(weatherUrl);
+
+  //       axios.get(weatherUrl).then((res) => {
+  //         console.log(typeof res.data.main.temp);
+
+  //         document.querySelector(".temperature").innerHTML =
+  //           Math.trunc(res.data.main.temp) - 273;
+  //         document.querySelector(".humidity").innerHTML =
+  //           res.data.main.humidity;
+  //         document.querySelector(".wind").innerHTML = res.data.wind.speed;
+  //         document.querySelector(
+  //           ".weather_icon"
+  //         ).src = `https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`;
+  //         setIsWeatherLoading(false);
+  //       });
+  //     });
+  // }, []);
 
   return (
     <>
@@ -88,7 +54,7 @@ function Weather() {
       <div className={`weather ${isWeatherLoading && "weather_hidden"}`}>
         <div className='weather_description'>
           <p>
-            Temp: <span className='inner_text temp'></span>°C
+            Temp: <span className='inner_text temperature'></span>°C
           </p>
           <p>
             Humidity: <span className='inner_text humidity'></span>%
